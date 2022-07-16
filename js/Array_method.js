6fd6679cbd0ad224995ccc7e22e63046bc4a78e9
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-07-16 22:53:12 +0700

    first commit

--- js/Array_method.js
+++ js/Array_method.js
@@ -143,6 +143,13 @@ console.table({
 
 function Getpos(position) {
   var pos = position.coords;
+  console.log('vĩ độ : ',pos.latitude);
+  console.log('kinh độ : ',pos.longitude);
   return 'vĩ độ : ',pos.latitude+'kinh độ : ',pos.longitude;
 }
 let pos_place = navigator.geolocation.getCurrentPosition(Getpos);
+
+
+
+
+
