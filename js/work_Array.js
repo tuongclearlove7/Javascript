c9d13d4e92014ef1b6a958413a6cb80c185606c0
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-01-30 22:04:30 +0700

    update code
    Commit update code

--- js/work_Array.js
+++ js/work_Array.js
@@ -153,9 +153,33 @@ function remove(index) {
     return array.slice(0, index).concat(array.slice(index + 1));
 }
 
-
-
 console.log(remove(2));
+
+function countBy(items, groupName) {
+
+    let counts = [];
+
+    for (let item of items) {
+
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+
+        if (known == -1) {
+
+            counts.push({name, count: 1});
+
+        } 
+
+        else {
+            
+            counts[known].count++;
+        }
+    }
+
+    return counts;
+}
+
+console.log(countBy([1, 2, 3, 4], n => n > 1));
 
 
 
